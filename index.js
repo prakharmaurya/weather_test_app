e5dbcb6327fca8c22b7cba22115d90596391c3f3
Excel <000.excel@gmail.com>
2020-09-16 10:19:36 +0530

    code converted to es5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const divHtml = document.getElementById("div");
-const data = ["temp", "pm25", "o3", "humidity", "wind_direction", "wind_gust"];
-const icons = [
+"use strict";
+
+var divHtml = document.getElementById("div");
+var data = ["temp", "pm25", "o3", "humidity", "wind_direction", "wind_gust"];
+var icons = [
   "wb_sunny",
   "settings",
   "wb_sunny",
@@ -9,15 +11,19 @@ const icons = [
   "wb_sunny",
 ];
 
-const htmlPrinter = (res) => {
+var htmlPrinter = function htmlPrinter(res) {
   console.log(res.data);
 
-  for (let i = 0; i < data.length; i++) {
-    const node = document.createElement("p");
-    const icon = document.createElement("i");
-    const iconNode = document.createTextNode(icons[i]);
-    const textNode = document.createTextNode(
-      `${data[i]} : ${res.data[0][data[i]].value} ${res.data[0][data[i]].units}`
+  for (var i = 0; i < data.length; i++) {
+    var node = document.createElement("p");
+    var icon = document.createElement("i");
+    var iconNode = document.createTextNode(icons[i]);
+    var textNode = document.createTextNode(
+      data[i] +
+        " : " +
+        res.data[0][data[i]].value +
+        " " +
+        res.data[0][data[i]].units
     );
     node.appendChild(textNode);
     icon.appendChild(iconNode);
@@ -27,19 +33,23 @@ const htmlPrinter = (res) => {
   }
 };
 
-const getLocation = () => {
+var getLocation = function getLocation() {
   navigator.geolocation.getCurrentPosition(
-    (res) => {
+    function (res) {
       console.log(res.coords.latitude, res.coords.longitude);
       rqurst(res.coords.latitude, res.coords.longitude);
     },
-    () => {}
+    function () {}
   );
 };
 
-const rqurst = (lat, lon) => {
-  const r = axios(
-    `https://api.climacell.co/v3/weather/nowcast?apikey=aZLoqsu6V8dVLc5iG86QhX1qLpgbGabc&unit_system=si&timestep=5&start_time=now&lat=${lat}&lon=${lon}&fields=temp,feels_like,dewpoint,humidity,wind_speed,wind_direction,wind_gust,baro_pressure,precipitation,precipitation_type,sunrise,sunset,visibility,pm25,o3,no2,co`
+var rqurst = function rqurst(lat, lon) {
+  var r = axios(
+    "https://api.climacell.co/v3/weather/nowcast?apikey=aZLoqsu6V8dVLc5iG86QhX1qLpgbGabc&unit_system=si&timestep=5&start_time=now&lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&fields=temp,feels_like,dewpoint,humidity,wind_speed,wind_direction,wind_gust,baro_pressure,precipitation,precipitation_type,sunrise,sunset,visibility,pm25,o3,no2,co"
   );
   r.then(htmlPrinter);
 };
